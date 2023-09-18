04e6ca01f718d9d07dd805a98c266f5e435f1b13
Pankaj Agade <pankaj.agade@htstechsolutions.com>
2023-09-18 17:56:42 +0530

    some changes

--- routes/Payment/customerPayment.js
+++ routes/Payment/customerPayment.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var easebuzzConfig = require("../../configs/easebuzzConfig").config;
+var supabaseInstance = require("../../services/supabaseClient").supabase;
+
 
 const axios = require('axios').default;
 const { URLSearchParams } = require('url');
@@ -14,74 +16,85 @@ router.get('/', (req, res, next) => {
 
 router.post('/initiate-payment', async (req, res, next) => {
 
-    const txnid = 'EBZTestTxn0005';
-    const amount = '2.1';
-    const productinfo = 'sample productinfo';
-    const firstname = 'Pankaj';
-    const phone = '9966338855';
-    const email = 'pankaj@gmail.com';
-    const surl = 'http://localhost:8000/payment/customer/success-payment';
-    const furl = 'http://localhost:8000/payment/customer/failure-payment';
+    const {amount,productinfo,firstname,phone,email,customerAuthUID,outletId} =req.body;
 
-    const _generateHash = generateHash(txnid, amount, productinfo, firstname, email, "", "", "", "", "", "", "", "", "", "");
-    console.log("_generateHash => ", _generateHash);
+    const surl = `${req.protocol}://${req.get('host')}/payment/customer/success-payment`;
+    const furl = `${req.protocol}://${req.get('host')}/payment/customer/failure-payment`;
 
-    const encodedParams = new URLSearchParams();
-    encodedParams.set('key', easebuzzConfig.key);
-    encodedParams.set('txnid', txnid);
-    encodedParams.set('amount', amount);
-    encodedParams.set('productinfo', productinfo);
-    encodedParams.set('firstname', firstname);
-    encodedParams.set('phone', phone);
-    encodedParams.set('email', email);
-    encodedParams.set('surl', surl);
-    encodedParams.set('furl', furl);
-    encodedParams.set('hash', _generateHash);
-    encodedParams.set('udf1', '');
-    encodedParams.set('udf2', '');
-    encodedParams.set('udf3', '');
-    encodedParams.set('udf4', '');
-    encodedParams.set('udf5', '');
-    encodedParams.set('udf6', '');
-    encodedParams.set('udf7', '');
-    encodedParams.set('address1', '');
-    encodedParams.set('address2', '');
-    encodedParams.set('city', '');
-    encodedParams.set('state', '');
-    encodedParams.set('country', '');
-    encodedParams.set('zipcode', '');
-    // encodedParams.set('show_payment_mode', '');
-    // encodedParams.set('split_payments', '');
-    // encodedParams.set('request_flow', '');
-    // encodedParams.set('sub_merchant_id', '');
-    // encodedParams.set('payment category', '');
-    // encodedParams.set('account_no', '');
+    if( amount && productinfo && firstname && phone && email && customerAuthUID && outletId){
 
-    // console.log(encodedParams);
+        try {
+            let encodedParams = new URLSearchParams();
+            const transactionResponse = await supabaseInstance.from("Transaction").insert({amount,productinfo,firstname,phone,email,customerAuthUID,outletId}).select("*").maybeSingle();
+            
+            if(transactionResponse?.data){
+                console.log("transactionResponse=>",transactionResponse);
 
-    const options = {
-        method: 'POST',
-        url: `${easebuzzConfig.easebuzzBaseUrl}/payment/initiateLink`,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Accept: 'application/json'
-        },
-        data: encodedParams,
-    };
+                const _generateHash = generateHash(transactionResponse?.data?.txnid, amount, productinfo, firstname, email, "", "", "", "", "", "", "", "", "", "");
+                // console.log("_generateHash => ", _generateHash);
 
-    
-    console.log("initiateLink URL => ", `${easebuzzConfig.easebuzzBaseUrl}/payment/initiateLink`);
-    try {
-        const { data } = await axios.request(options);
-        if (data.status) {
-            console.log("data => ", data);
-            res.send({ success: true, data })
-        } else {
-            throw data;
+                encodedParams.set('key', easebuzzConfig.key);
+                encodedParams.set('txnid', transactionResponse?.data?.txnid);
+                encodedParams.set('amount', amount);
+                encodedParams.set('productinfo', productinfo);
+                encodedParams.set('firstname', firstname);
+                encodedParams.set('phone', phone);
+                encodedParams.set('email', email);
+                encodedParams.set('surl', surl);
+                encodedParams.set('furl', furl);
+                encodedParams.set('hash', _generateHash);
+                encodedParams.set('udf1', '');
+                encodedParams.set('udf2', '');
+                encodedParams.set('udf3', '');
+                encodedParams.set('udf4', '');
+                encodedParams.set('udf5', '');
+                encodedParams.set('udf6', '');
+                encodedParams.set('udf7', '');
+                encodedParams.set('address1', '');
+                encodedParams.set('address2', '');
+                encodedParams.set('city', '');
+                encodedParams.set('state', '');
+                encodedParams.set('country', '');
+                encodedParams.set('zipcode', '');
+                // encodedParams.set('show_payment_mode', '');
+                // encodedParams.set('split_payments', '');
+                // encodedParams.set('request_flow', '');
+                // encodedParams.set('sub_merchant_id', '');
+                // encodedParams.set('payment category', '');
+                // encodedParams.set('account_no', '');
+
+                // console.log(encodedParams);
+                
+                const options = {
+                    method: 'POST',
+                    url: `${easebuzzConfig.easebuzzBaseUrl}/payment/initiateLink`,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        Accept: 'application/json'
+                    },
+                    data: encodedParams,
+                };
+
+                axios.request(options).then(async (initiateLinkResponse) => {
+                    const transactionUpdateResponse = await supabaseInstance.from("Transaction").update({initiateLink_post_body: encodedParamsToObject(encodedParams), initiateLink_response: initiateLinkResponse.data}).eq("txnid", transactionResponse?.data?.txnid).select('*').maybeSingle();
+                    console.log("transactionUpdateResponse in then =>",transactionUpdateResponse);
+
+                    res.status(200).json({success: true, response:initiateLinkResponse?.data})
+                }).catch(async (error) => {
+                    console.error(error);
+                    const transactionUpdateResponse = await supabaseInstance.from("Transaction").update({initiateLink_post_body: encodedParamsToObject(encodedParams), initiateLink_error: error}).eq("txnid", transactionResponse?.data?.txnid).select('*').maybeSingle();
+                    console.log("transactionUpdateResponse in error=>",transactionUpdateResponse)
+                    res.status(200).json({success: false, response: error})
+                }) 
+            }else{
+                throw error
+            }
+        } catch (error) {
+            console.error("error => ", error);  
+            res.status(500).json({ success: false, error: error });
         }
-    } catch (error) {
-        console.error("error => ", error);
-        res.send({ success: false, error })
+    } else{
+        res.status(500).json({ success: false, error: "Invalid Post Body" });
     }
 })
 
@@ -122,4 +135,16 @@ const generateHash = (txnid, amount, productinfo, name, email, udf1, udf2, udf3,
     console.log("hashstring => ", hashstring);
 
     return SHA512(hashstring).toString();
+}
+
+function encodedParamsToObject(encodedParams) {
+    let obj = {};
+    const ent = encodedParams.entries();
+    // console.log("encodedParams.size => ", encodedParams.size);
+
+    for (let index = 0; index < encodedParams.size; index++) {
+        const element = ent.next();
+        obj[element.value[0]] = element.value[1];
+    }
+    return {...obj};
 }
