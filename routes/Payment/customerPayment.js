9a525e1fa6bffa94f6eebb8504bb80652177fce2
Pankaj Agade <pankaj.agade@htstechsolutions.com>
2023-10-26 12:19:23 +0530

    some

--- routes/Payment/customerPayment.js
+++ routes/Payment/customerPayment.js
@@ -132,6 +132,7 @@ router.post('/initiate-payment', async (req, res, next) => {
                                 [easebuzzConfig.mealpe_bank_label] : getPriceBreakdownResponse?.mealpeVendorAmount,
                                 [getPriceBreakdownResponse.outletBankLabel]: getPriceBreakdownResponse?.outletVendorAmount
                             }
+                            postBody.split_payments = JSON.stringify(postBody.split_payments);
                         }
         
                         // console.log(encodedParams);
