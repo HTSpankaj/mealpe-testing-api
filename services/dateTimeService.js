cea2f75edc85c667acbcec54189a2a75c2aa088d
Pankaj Agade <pankaj.agade@htstechsolutions.com>
2023-11-30 11:38:36 +0530

    ss

---
+++ services/dateTimeService.js
@@ -0,0 +1,38 @@
+const moment = require("moment-timezone");
+
+function formatTime(inputTime) {
+    const [hours, minutes, seconds] = inputTime.split('.').map(Number);
+    let formattedTime;
+    formattedTime = Number(`${hours}.${minutes}`);
+    return formattedTime;
+}
+
+function isTimeInRange(__time, __beforeTime, __afterTime) {
+    let time = moment(__time).format('HH:mm:ss')
+    let beforeTime = moment(__beforeTime).format('HH:mm:ss')
+    let afterTime = moment(__afterTime).format('HH:mm:ss')
+    let _selectedTime = formatTime(
+        time.replaceAll(':', '.'),
+    );
+    let _startTime = formatTime(beforeTime.replaceAll(':', '.'));
+    let _closeTime = formatTime(afterTime.replaceAll(':', '.'));
+    let temp = null;
+
+    _startTime = _startTime == 0 ? 24 : _startTime;
+    _closeTime = _closeTime == 0 ? 24 : _closeTime;
+
+
+    if (_closeTime < _startTime) {
+        temp = _startTime;
+        _startTime = _closeTime;
+        _closeTime = temp;
+    }
+
+    const isBetween =
+        _selectedTime >= _startTime && _selectedTime <= _closeTime;
+    return isBetween
+}
+
+module.exports = {
+    isTimeInRange
+}
