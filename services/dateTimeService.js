c3506eeaff05385ce5e2381e9ea0da7527a91bef
Pankaj Agade <pankaj.agade@htstechsolutions.com>
2023-11-30 12:30:41 +0530

    s

--- services/dateTimeService.js
+++ services/dateTimeService.js
@@ -12,10 +12,10 @@ function isTimeInRange(__time, __beforeTime, __afterTime) {
     let beforeTime = moment(__beforeTime).format('HH:mm:ss')
     let afterTime = moment(__afterTime).format('HH:mm:ss')
     let _selectedTime = formatTime(
-        time.replaceAll(':', '.'),
+        time.replace(/:/g, '.'),
     );
-    let _startTime = formatTime(beforeTime.replaceAll(':', '.'));
-    let _closeTime = formatTime(afterTime.replaceAll(':', '.'));
+    let _startTime = formatTime(beforeTime.replace(/:/g, '.'));
+    let _closeTime = formatTime(afterTime.replace(/:/g, '.'));
     let temp = null;
 
     _startTime = _startTime == 0 ? 24 : _startTime;
