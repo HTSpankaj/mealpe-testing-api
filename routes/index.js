bda6b64c48e5e99f3de26e5baea63591615f5bfc
Pankaj Agade <pankaj.agade@htstechsolutions.com>
2023-12-12 12:00:04 +0530

    some changes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(`Protocol ==> ${req.protocol}://${req.get('host')}/`);
+  console.log("req.secure => ", req.secure)
   res.render('index', { title: 'Express' });
 });
 
