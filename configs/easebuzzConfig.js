47e117164948daf23e6c6559d615be1042009aaf
Pankaj Agade <pankaj.agade@htstechsolutions.com>
2024-01-15 13:58:09 +0530

    s

--- configs/easebuzzConfig.js
+++ configs/easebuzzConfig.js
@@ -1,8 +1,11 @@
 const config = {
     easebuzzBaseUrl: "https://testpay.easebuzz.in",
     refund_easebuzzBaseUrl: "https://testdashboard.easebuzz.in",
-    key: "2PBP7IABZ2",
-    salt: "DAH88E3UWQ",
+    transaction_API:"https://testdashboard.easebuzz.in",
+    // key: "2PBP7IABZ2",
+    key: "PN3DY31G6",
+    // salt: "DAH88E3UWQ",
+    salt: "PGDKQXXY0",
     mealpe_bank_label :""
 };
 
